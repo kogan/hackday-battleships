8972c1f94586da5078455964bba902590da593ac
David <smith.d.mon@gmail.com>
2020-05-08 15:22:24 +1000

    Feature/spectate (#14)
    PlayerBoard can display board state from a moves list

--- admirals-bridge/src/components/Spectate/index.js
+++ admirals-bridge/src/components/Spectate/index.js
@@ -1,5 +1,92 @@
 import React from "react";
+import styled from 'styled-components'
+
+const HitCell = styled.div`
+  background-color: red;
+  width: 100%;
+  height: 100%;
+`
+const MissedCell = styled.div`
+  background-color: blue;
+  width: 100%;
+  height: 100%;
+`
+const SunkCell = styled.div`
+  background-color: black;
+  width: 100%;
+  height: 100%;
+`
+const DefaultCell = styled.div`
+    background-color: white;
+    width: 100%;
+    height: 100%;
+`
+
+const GameGrid = styled.div`
+    display: grid;
+    background-color: grey;
+    border: 5px solid grey;
+    border-radius: 5px;
+    grid-template-columns: repeat(10, 1fr);
+    grid-template-rows: repeat(10, 1fr);
+    width: 500px;
+    height: 500px;
+    grid-gap: 5px;
+`
+
 
 const Spectate = () => <div>Spectate</div>;
 
-export default Spectate;
+function ResultCell({result}) {
+    let comp = null;
+    switch (result) {
+        case "HIT":
+            comp = <HitCell />
+            break;
+        case "MISS":
+            comp = <MissedCell />
+            break;
+        case "SUNK":
+            comp = <SunkCell />
+            break;
+        default:
+            comp = <DefaultCell />
+    }
+    return (comp)
+}
+
+function displayResult(result) {
+    return <ResultCell result={result}/>
+}
+
+function DisplayPlayer({moves}) {
+    return (
+        <GameGrid>{
+            moves.map(displayResult)
+        }
+        </GameGrid>
+    )
+}
+
+function stateToBoard(state, player) {
+    let results = []
+    for(let x = 0; x < 10; x++) {
+        for(let y = 0; y < 10; y++) {
+        let toAppend = state.filter((item) => item.x == x && item.y == y && item.player == player)
+        if (toAppend.length === 1){
+            results.push(toAppend[0].result)
+            }
+        else {
+            results.push(null)
+        }
+        }
+    }
+    return results
+}
+
+function PlayerBoard({props}) {
+    let playerMoves = stateToBoard(props.moves, props.player)
+    return <DisplayPlayer moves={playerMoves}/>
+}
+
+export {Spectate, ResultCell, DisplayPlayer, stateToBoard, PlayerBoard};

237ee8a574a5712dc5182593257b90ce496a4a0e
rnkdev <riznkadir@gmail.com>
2020-05-08 15:37:20 +1000

    update matchmaker and api

--- admirals-bridge/src/api.js
+++ admirals-bridge/src/api.js
@@ -1,6 +1,29 @@
 const BASE_URL = "http://localhost:8645";
 export const URLMAP = {
   PLAYERS: `${BASE_URL}/api/player/`,
+  BATTLE: `${BASE_URL}/api/battle/`,
 };
 
 export const ADMIN_API_TOKEN = process.env.ADMIN_API_TOKEN || "636f47ef285834acb5c729d012a20c56b0107b3c";
+
+
+export const battleRequest = (player1, player2) => {
+  return fetch(URLMAP["BATTLE"], {
+    Authorization: `Token ${ADMIN_API_TOKEN}`,
+    method: 'POST',
+    body: JSON.stringify({
+      player1: player1,
+      player2, player2,
+    })
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Failure triggering battle")
+    }
+    return Promise.resolve(true)
+  })
+  .catch(error => {
+    console.error("Failure triggering battle")
+    return Promise.resolve(false)
+  });
+}
--- admirals-bridge/src/components/MatchMaker/index.js
+++ admirals-bridge/src/components/MatchMaker/index.js
@@ -1,18 +1,59 @@
-import React from "react";
+import React, {Fragment} from "react";
+import styled from "styled-components";
+import Button from "../Button";
+import {battleRequest} from '../../api.js'
+import { Link } from "react-router-dom";
 import { StateContext } from "../../App";
+
+const VersusHeader = styled.h1`
+  && {
+    margin: 0 auto;
+    text-align: center;
+    margin-bottom: 12px;
+
+    span {
+      font-size: 24px;
+    }
+  }
+`
+
 
 const MatchMaker = () => {
   const {selectedPlayers, config} = React.useContext(StateContext);
+  const [error, setError] = React.useState("");
+
+  const onBattleClick = () => {
+    battleRequest(selectedPlayers[0], selectedPlayers[1]).then(result => {
+      if (result) {
+        console.log("Battle successfully launched")
+      } else {
+        setError("Failure in launching battle.. Please see error")
+      }
+    })
+  }
 
   if (selectedPlayers.length == 0) {
-    return <div>Please select player to battle in the settings first.</div>
+    return (
+      <Fragment>
+        Please select player to battle in the settings first.
+        <div>
+          <Link to="/settings/">
+            <Button style={{marginTop: '12px'}}>Settings</Button>
+          </Link>
+        </div>
+      </Fragment>
+    )
   }
 
   return (
     <div>
-      <h1>{selectedPlayers[0]} vs {selectedPlayers[1]}</h1>
+      <VersusHeader>{selectedPlayers[0]} <span>vs</span> {selectedPlayers[1]}</VersusHeader>
       Prepare Your Battleships!
       Ready to launch {config.turns} turns battle.
+      <div>
+        <Button onClick={onBattleClick} style={{marginTop: '12px'}}>Battle!</Button>
+      </div>
+      <p style={{color: 'red'}}>{error}</p>
     </div>
   )
 }

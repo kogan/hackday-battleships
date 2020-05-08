44041a4231760cf8b20b348a7f949b2b4d9bfaf7
rnkdev <riznkadir@gmail.com>
2020-05-08 14:49:52 +1000

    update token

--- admirals-bridge/src/api.js
+++ admirals-bridge/src/api.js
@@ -2,3 +2,5 @@ const BASE_URL = "http://localhost:8645";
 export const URLMAP = {
   PLAYERS: `${BASE_URL}/api/player/`,
 };
+
+export const ADMIN_API_TOKEN = process.env.ADMIN_API_TOKEN || "636f47ef285834acb5c729d012a20c56b0107b3c";

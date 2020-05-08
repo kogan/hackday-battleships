a0d33b6b3562de55c54aa70e3f5dc22d2e48c525
Josh Smeaton <josh.smeaton@gmail.com>
2020-05-08 12:28:20 +1000

    Reprs and empty board

--- hottest100/engine.py
+++ hottest100/engine.py
@@ -10,9 +10,9 @@ class Orientation(enum.Enum):
 
 
 class State(enum.Enum):
-    Unknown = "unknown"
-    Hit = "hit"
-    Empty = "empty"
+    Unknown = "?"
+    Hit = "X"
+    Empty = "O"
 
 
 @dataclass
@@ -20,11 +20,17 @@ class Point:
     x: int
     y: int
 
+    def __repr__(self):
+        return f"({self.x},{self.y})"
+
 
 @dataclass
 class Tile:
     position: Point
     state: State
+
+    def __repr__(self):
+        return f"{self.position}: {self.state.value}"
 
 
 @dataclass
@@ -37,13 +43,21 @@ class Ship:
 @dataclass
 class Board:
     tiles: t.List[t.List[Tile]]
+    size: int = 10
+
+    @classmethod
+    def empty(cls, size: int):
+        tiles = [
+            [Tile(Point(y, x), state=State.Unknown) for x in range(size)]
+            for y in range(size)
+        ]
+        return Board(tiles=tiles, size=size)
 
 
 @dataclass
 class Engine:
-    our_board: Board
-    opponent_board: Board
-    test_board: Board
+    our_board: t.Optional[Board] = None
+    opponent_board: t.Optional[Board] = None
     size: int = 10
 
     def generate_board(self) -> Board:
@@ -57,3 +71,17 @@ class Engine:
     def test(self):
         self.our_board = self.generate_board()
         self.test_board = self.generate_board()
+
+
+class EngineRunner:
+    def __init__(self, board_size: int = 10):
+        self.engine = Engine(size=board_size)
+        self.opponent_board = self.engine.generate_board()
+
+    def test(self):
+        pass
+
+
+if __name__ == "__main__":
+    runner = EngineRunner()
+    runner.test()

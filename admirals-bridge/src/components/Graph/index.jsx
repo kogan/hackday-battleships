765bf509fdfede4b96c7a1af368b07dc2ff1936a
Kieran <kieran.lomas@kogan.com.au>
2020-05-08 15:09:52 +1000

    Update plot styles

--- admirals-bridge/src/components/Graph/index.jsx
+++ admirals-bridge/src/components/Graph/index.jsx
@@ -20,6 +20,13 @@ export default class Graph extends React.Component {
           },
           yaxis: {
             rangemode: "nonnegative"
+          },
+          paper_bgcolor: "rgba(0,0,0,0)",
+          plot_bgcolor: "rgba(0,0,0,0)",
+          font: {
+            color: "#999",
+            family: `"Righteous", Roboto, "Open Sans", arial, sans-serif`
+
           }
         }}
         style={{ width: '100%', height: '100%'}}

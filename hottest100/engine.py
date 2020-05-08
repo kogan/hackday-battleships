caaf2672645b2a3070053aff5714c7d08a0c51a0
Josh Smeaton <josh.smeaton@gmail.com>
2020-05-08 12:54:33 +1000

    Engine and Coordinator guts are going

--- hottest100/engine.py
+++ hottest100/engine.py
@@ -47,18 +47,34 @@ class Board:
 
     @classmethod
     def empty(cls, size: int):
-        tiles = [
-            [Tile(Point(y, x), state=State.Unknown) for x in range(size)]
-            for y in range(size)
-        ]
+        tiles = [[Tile(Point(y, x), state=State.Unknown) for x in range(size)] for y in range(size)]
         return Board(tiles=tiles, size=size)
+
+
+class AttackResponse(enum.Enum):
+    Hit = "hit"
+    Miss = "miss"
+    Sunk = "sunk"
+    Win = "win"
+
+
+class Coordinator:
+    def attack(self, point: Point) -> AttackResponse:
+        return AttackResponse.Win
+
+    def set_placement(self, board: Board) -> bool:
+        return True
 
 
 @dataclass
 class Engine:
+    opponent_board: Board
     our_board: t.Optional[Board] = None
-    opponent_board: t.Optional[Board] = None
     size: int = 10
+
+    def __init__(self, size=10):
+        self.size = size
+        self.opponent_board = Board.empty(size=size)
 
     def generate_board(self) -> Board:
         self.our_board = Board(tiles=[])
@@ -72,16 +88,37 @@ class Engine:
         self.our_board = self.generate_board()
         self.test_board = self.generate_board()
 
+    def play(self, coordinator: Coordinator):
+        response = AttackResponse.Miss
+        while response != AttackResponse.Win:
+            attack = self.get_attack()
+            response = coordinator.attack(self.get_attack())
+            tile = self.opponent_board.tiles[attack.x][attack.y]
+            if response is AttackResponse.Hit:
+                tile.state = State.Hit
+            elif response is AttackResponse.Sunk:
+                tile.state = State.Hit
+            elif response is AttackResponse.Win:
+                tile.state = State.Hit
+            elif response is AttackResponse.Miss:
+                tile.state = State.Empty
 
-class EngineRunner:
-    def __init__(self, board_size: int = 10):
-        self.engine = Engine(size=board_size)
-        self.opponent_board = self.engine.generate_board()
 
-    def test(self):
-        pass
+class TestCoordinator(Coordinator):
+    def __init__(self, engine: Engine):
+        self.engine = engine
+
+    def attack(self, point: Point) -> AttackResponse:
+        response = AttackResponse.Win
+        print(f"Attack: {point} | {response}")
+        return response
+
+    def set_placement(self, board: Board) -> bool:
+        return True
 
 
 if __name__ == "__main__":
-    runner = EngineRunner()
-    runner.test()
+    size = 10
+    engine = Engine(size=size)
+    coordinator = TestCoordinator(engine)
+    engine.play(coordinator)

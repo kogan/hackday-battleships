c92261a7f4eecc614ed22d438457d612a1f49a37
rnkdev <riznkadir@gmail.com>
2020-05-08 12:04:43 +1000

    Lobby Buttons

--- admirals-bridge/src/components/Lobby/index.js
+++ admirals-bridge/src/components/Lobby/index.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, {Fragment} from "react";
+import { Link } from "react-router-dom";
+import styled from 'styled-components'
 
-const Lobby = () => <div>Lobby</div>;
+const MenuButton = styled.div`
+  border: solid 3px #40a692;
+  background-color: rgba(21, 30, 27, 0.9);
+  padding: 12px;
+  color: white;
+  width: 50%;
+  margin: 12px auto;
+
+  &:hover {
+    background-color: #40a692;
+  }
+`
+
+const Lobby = () => (
+  <Fragment>
+    <Link to="/match/">
+      <MenuButton>Start</MenuButton>
+    </Link>
+    <Link to="/results/">
+      <MenuButton>Battle Results</MenuButton>
+    </Link>
+    <MenuButton>Settings</MenuButton>
+  </Fragment>
+)
 
 export default Lobby;
--- admirals-bridge/src/container/LobbyContainer/index.js
+++ admirals-bridge/src/container/LobbyContainer/index.js
@@ -1,10 +1,24 @@
 import React from "react";
+import styled from 'styled-components'
+
+const ScreenBackground = styled.div`
+  max-width: 619px;
+  border: solid 3px #40a692;
+  background-color: rgba(21, 30, 27, 0.9);
+  padding: 12px;
+  margin: 0 auto;
+  margin-top: 100px;
+
+  h1 {
+    color: white;
+  }
+`
 
 const LobbyContainer = ({children}) => (
-  <div>
+  <ScreenBackground>
     <h1>Battleship</h1>
     {children}
-  </div>
+  </ScreenBackground>
 )
 
 export default LobbyContainer;

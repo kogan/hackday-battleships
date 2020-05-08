3f34dbe37853c30a63e1e487e998371b5400cbe3
Josh Smeaton <josh.smeaton@gmail.com>
2020-05-08 23:34:12 +1000

    Move interface into coordinator system

--- hottest100/app.py
+++ hottest100/app.py
@@ -3,7 +3,7 @@ import os
 import requests
 from flask import Flask, jsonify, request
 
-from engine import attack, setup, wait
+from engine import HttpCoordinator
 
 
 def create_app():
@@ -12,19 +12,15 @@ def create_app():
     @app.route("/", methods=["POST"])
     def start_game():
         try:
+            print("start game request")
             data = request.json
             token = os.environ.get("GAME_TOKEN")
             url = data["url"]
             game_id = data["game_id"]
             session = requests.Session()
             session.headers.update(dict(Authorization=f"Token {token}"))
-            print(f"Starting game: {game_id=} {url}")
-            ships, config = setup(session, url, game_id)
-            print(f"Setup done: {game_id=} {ships} {config} {url}")
-            wait(session, url, game_id, "attack")
-            print(f"Beginning attack: {game_id=}")
-            attack(session, url, game_id, config)
-            print(f"Complete: {game_id=}")
+            coordinator = HttpCoordinator(session, url, game_id)
+            coordinator.run()
             return jsonify(success=True)
         except Exception as ex:
             print("Error: ", str(ex))
